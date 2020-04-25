f8385eb7191c2d19eccfcd9aa3c12b41921b3f8a
fedeghe <fedeghe@gmail.com>
2020-04-25 22:33:21 +0200

    fork

--- samples/sample_list.php
+++ samples/sample_list.php
@@ -59,3 +59,8 @@ if($prev)echo '<a href="'.$prev.'">&lArr; previous sample</a>';
 if($prev && $next)echo ' | ';
 if($next)echo '<a href="'.$next.'">next sample &rArr;</a>';
 ?>
+<div style="position:absolute;right:0px;top:0px">
+    <a href="https://github.com/fedeghe/gmapsimple">
+        <img width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_green_007200.png?resize=149%2C149" class="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1">
+    </a>
+</div>
